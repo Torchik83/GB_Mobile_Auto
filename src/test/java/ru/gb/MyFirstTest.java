b4dc0c607497cc1870f88f566ccfaed6d82ea909
Vladimir <vvk2383@gmail.com>
2022-03-07 11:02:56 +0300

    first test

--- src/test/java/ru/gb/MyFirstTest.java
+++ src/test/java/ru/gb/MyFirstTest.java
@@ -34,5 +34,30 @@ public class MyFirstTest {
 //      Проверяем текст ошибки.
         MobileElement errorText = (MobileElement) driver.findElementByXPath("//android.widget.ScrollView[@content-desc=\"Login-screen\"]/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup[4]/android.widget.TextView[1]");
         Assert.assertEquals(errorText.getText(), "Please enter a valid email address");
+//      Проверка вкладки "Forms"
+        MobileElement formsButton = (MobileElement) driver.findElementByXPath("//android.view.ViewGroup[@content-desc=\"Forms\"]/android.view.ViewGroup/android.widget.TextView");
+        formsButton.click();
+        Thread.sleep(2000);
+//      Проверка элементов на вкладке "Forms"
+        MobileElement checkText = (MobileElement) driver.findElementByXPath("//android.widget.ScrollView[@content-desc=\"Forms-screen\"]/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup[2]/android.widget.TextView[1]");
+        Assert.assertEquals(checkText.getText(), "Input field");
+        MobileElement checkText2 = (MobileElement) driver.findElementByXPath("//android.widget.ScrollView[@content-desc=\"Forms-screen\"]/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup[2]/android.widget.TextView[4]");
+        Assert.assertEquals(checkText2.getText(), "Switch");
+        MobileElement checkText3 = (MobileElement) driver.findElementByXPath("//android.view.ViewGroup[@content-desc=\"select-Dropdown\"]/android.widget.TextView[1]");
+        Assert.assertEquals(checkText3.getText(), "Dropdown");
+        Thread.sleep(2000);
+//      Нажатие на конопку "Activate"
+        MobileElement activateButton = (MobileElement) driver.findElementByXPath("//android.view.ViewGroup[@content-desc=\"button-Active\"]/android.view.ViewGroup");
+        activateButton.click();
+        Thread.sleep(2000);
+//      Проверка надписи "This button is active"
+        MobileElement visibleText = (MobileElement) driver.findElementByXPath("/hierarchy/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.ScrollView/android.widget.LinearLayout/android.widget.TextView");
+        Assert.assertEquals(visibleText.getText(), "This button is active");
+//      Нажатие на кнопку "OK"
+        MobileElement okButton = (MobileElement) driver.findElementById("android:id/button1");
+        okButton.click();
+//      Проверка выхода в окно  "Form components"
+        MobileElement formText = (MobileElement) driver.findElementByXPath("//android.widget.ScrollView[@content-desc=\"Forms-screen\"]/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup[1]/android.widget.TextView");
+        Assert.assertEquals(formText.getText(), "Form components");
     }
 }
